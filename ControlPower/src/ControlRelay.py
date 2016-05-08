795c70327da371b75895e09fadc6ff24855bea76
Moxiaoyong <1987mxy@gmail.com>
2016-05-09 01:08:18 +0800

    初始化
    初始化，把文件都集中在一起。

---
+++ ControlPower/src/ControlRelay.py
@@ -0,0 +1,89 @@
+#encoding=utf-8
+'''
+Created on 2014年2月24日
+
+@author: Moxiaoyong
+'''
+
+import serial
+from time import sleep
+from struct import pack, unpack
+
+moduleAddress = 1
+portNumber = 2
+
+class ControlRelay:
+	def __init__(self, module, port):
+		self.module = module
+		self.port = port - 1
+		self.package = None
+		self.comm = None
+		self.connect()
+
+	def connect(self):
+		if self.comm == None:
+			self.comm = serial.Serial(portNumber)
+	
+	def send(self):
+		self.comm.write(self.package)
+		
+	def receive(self):
+		package = self.comm.read(serial.EIGHTBITS)
+		data = unpack('BBBBBBBB', package)
+		return data
+	
+	def reset(self):
+		self.package = None
+	
+	def close(self):
+		self.comm.close()
+		self.comm = None;
+		
+	def genPackage(self, packageType, data):
+		packageData = [0x55,
+						self.module % 256,
+						packageType]
+		packageData += data
+		packageSum = sum(packageData)
+		packageData.append(packageSum % 256)
+		self.package = pack('<BBBBBBBB', *packageData)
+	
+	def allPowerOn(self):
+		data = [0] * 4
+		data[2:] = [0xff, 0xff]
+		self.genPackage(0x13, data)
+		self.send()
+		self.reset()
+	
+	def allPowerOff(self):
+		data = [0] * 4
+		self.genPackage(0x13, data)
+		self.send()
+		self.reset()
+	
+	def status(self):
+		data = [0] * 4
+		self.genPackage(0x10, data)
+		self.send()
+		self.reset()
+		data = self.receive()
+		
+	def singlePowerOn(self, linkNum):
+		data = [0] * 4
+		data[linkNum - 1] = 2
+		self.genPackage(0x01, data)
+		print self.package.__repr__()
+		self.send()
+		self.reset()
+	
+	def singlePowerOff(self, linkNum):
+		data = [0] * 4
+		data[linkNum - 1] = 1
+		self.genPackage(0x01, data)
+		self.send()
+		self.reset()
+
+if __name__ == '__main__':
+	comm = ControlRelay(1, 3)
+	comm.allPowerOn()
+	comm.allPowerOff()
---
+++ MyProtector/WifiCar/src/resource/js/car.mobile.js
@@ -0,0 +1,47 @@
+/**
+ * 
+ */
+
+var lock = 'x';
+$(function() {
+	// $('.direct-btn').touchstart(function() {
+	// 	var direct = $(this).attr('id');
+	// 	if ((direct == 'left' || direct == 'right') && xLock == '') {
+	// 		$(this).button('toggle');
+	// 		run(direct);
+	// 		xLock = direct;
+	// 	} else if ((direct == 'up' || direct == 'down') && yLock == '') {
+	// 		$(this).button('toggle');
+	// 		run(direct);
+	// 		yLock = direct;
+	// 	}
+	// }).touchend(function() {
+	// 	var direct = $(this).attr('id');
+	// 	if ((direct == 'left' || direct == 'right') && xLock == direct) {
+	// 		$(this).button('toggle');
+	// 		stop(direct);
+	// 		xLock = '';
+	// 	} else if ((direct == 'up' || direct == 'down') && yLock == direct) {
+	// 		$(this).button('toggle');
+	// 		stop(direct);
+	// 		yLock = '';
+	// 	}
+	// });
+
+	$('body').swipeleft(function() {
+		if (lock == '') {
+			run('left');
+			lock = 'x';
+		}
+	}).swiperight(function() {
+		if (lock == '') {
+			run('right');
+			lock = 'x';
+		}
+	}).touchend(function() {
+		if (lock != '') {
+			stop(lock);
+			lock = '';
+		}
+	});
+});
